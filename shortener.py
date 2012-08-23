c3726cc86f3a435fcd1b41a485c17166d8ac21a9
Ivan Pedrazas <ipedrazas@gmail.com>
2012-08-23 18:53:07 +0100

    stats added

--- shortener.py
+++ shortener.py
@@ -7,16 +7,21 @@ from pymongo import Connection
 from datetime import datetime
 import os
 
-URL = os.environ['URL']
-DB_URL = os.environ['DB_URL']
+
+try:
+    URL = os.environ['URL']
+    DB_URL = os.environ['DB_URL']
+    NUM_CHARS = os.environ['NUM_CHARS']
+except:
+    URL = 'http://localhost:5000/'
+    DB_URL = 'mongodb://localhost:27017'
+    NUM_CHARS = 3
+
 
 conn = Connection(DB_URL)
-
 db = conn['shurls']
 objects = db['urls']
-num = 3
-
-BASE = URL
+num = int(NUM_CHARS)
 
 
 @app.route('/', methods=['GET'])
@@ -24,12 +29,24 @@ def hello_world():
     return 'Url Shortener made with MongoDB, Flask and python, of course!'
 
 
+@app.route('/stats/<url_id>', methods=['GET'])
+def stats(url_id=None):
+    if url_id:
+        url = objects.find_one({'url_id': url_id})
+        if url:
+            url['id'] = str(url['_id'])
+            url['added'] = date_to_str(url['added'])
+            del url['_id']
+            return jsonify({'stats': url})
+
+
 @app.route('/<url_id>', methods=['GET'])
 def get(url_id=None):
-    url = objects.find_one({'url_id': url_id})
-    if url:
-        objects.update({'_id': url['_id']}, {'$inc': {'hits': 1}})
-        return redirect(url['link'], 301)
+    if url_id:
+        url = objects.find_one({'url_id': url_id})
+        if url:
+            objects.update({'_id': url['_id']}, {'$inc': {'hits': 1}})
+            return redirect(url['link'], 301)
     return abort(404)
 
 
@@ -45,12 +62,17 @@ def add():
     url = objects.find_one({'link': link})
     if url:
         objects.update({'_id': url['_id']}, {'$inc': {'saved': 1}})
-        return jsonify({'url': BASE + url['url_id']})
+        return jsonify({'url': URL + url['url_id']})
     else:
-        return jsonify({'url': BASE + short_id(link)})
+        return jsonify({'url': URL + short_id(link)})
+
+
+def date_to_str(obj):
+    if isinstance(obj, datetime):
+        return obj.strftime('%d-%m-%Y- %H:%M:%S')
+
 
 if __name__ == '__main__':
      # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
